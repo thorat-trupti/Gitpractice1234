6e9b87cb486ee1881cb49c5a6b44ad3791e08739
thorat-trupti <thoratrupti99@gmail.com>
2024-04-01 17:53:09 +0530

    changes by trupti

--- Springboot_Crud-1/src/main/java/com/cjc/webapp/crud/SpringbootCrud1Application.java
+++ Springboot_Crud-1/src/main/java/com/cjc/webapp/crud/SpringbootCrud1Application.java
@@ -8,6 +8,7 @@ public class SpringbootCrud1Application {
 
 	public static void main(String[] args) {
 		System.out.println("This is springboot Curd1234");
+		System.out.println("practice stash");
 		SpringApplication.run(SpringbootCrud1Application.class, args);
 	}
 
